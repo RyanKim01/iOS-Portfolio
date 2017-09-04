7b496ff27c451cc6272563b631a319a29e5c4b8c
RyanKim01 <kyhekm@hotmail.com>
2017-09-03 17:59:21 -0700

    Implement a collectionview within a collectionviewcell

---
+++ 5. AppStoreDemo/AppStoreDemo/CategoryCell.swift
@@ -0,0 +1,94 @@
+//
+//  CategoryCell.swift
+//  AppStoreDemo
+//
+//  Created by Ryan Kim on 9/3/17.
+//  Copyright © 2017 RKProduction. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let appsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .clear
+        
+        return collectionView
+    }()
+    
+    fileprivate let cellId = "appCellId"
+    
+    func setupViews() {
+        backgroundColor = .clear
+        addSubview(appsCollectionView)
+        
+        appsCollectionView.delegate = self
+        appsCollectionView.dataSource = self
+        appsCollectionView.register(AppCell.self, forCellWithReuseIdentifier: cellId)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
+    }
+}
+
+extension CategoryCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let appCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! AppCell
+        return appCell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: frame.height)
+    }
+}
+
+class AppCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let imageView: UIImageView = {
+       let iv = UIImageView()
+        iv.image = UIImage(named: "picasso")
+        iv.layer.cornerRadius = 16
+        iv.layer.masksToBounds = true
+        iv.contentMode = .scaleAspectFill
+        return iv
+    }()
+    
+    func setupViews() {
+        addSubview(imageView)
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.width)
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ 5. AppStoreDemo/AppStoreDemo/FeaturedAppsVC.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  AppStoreDemo
+//
+//  Created by Ryan Kim on 9/3/17.
+//  Copyright © 2017 RKProduction. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedAppsVC: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    private let cellID = "categoryCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView?.backgroundColor = .white
+        collectionView?.register(CategoryCell.self, forCellWithReuseIdentifier: cellID)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! CategoryCell
+        return cell
+    }
+    
+    //flow layout delegate
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
